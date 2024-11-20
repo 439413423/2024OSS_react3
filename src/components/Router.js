18b4d415e8b348242f21bec06a4e0fef22a596e6
439413423 <22100582@handong.ac.kr>
2024-11-20 14:24:19 +0900

    .

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,15 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
 import ShowList from "./components/Pages/ShowList.js";
 import EditUser from "./components/User/EditUser.js";
-import Detail from "./components/Pages/Detail.js"; 
+import Detail from "./components/Pages/Detail.js";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-
         <Route path="/" element={<Navigate to="/list" replace />} />
         <Route path="/list" element={<ShowList />} />
         <Route path="/detail" element={<Detail />} />
--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css"; 
 import "bootstrap/dist/css/bootstrap.min.css"; 
-import "bootstrap/dist/js/bootstrap.bundle.min";
-import ShowList from "./components/Pages/ShowList";
+import "bootstrap/dist/js/bootstrap.bundle.min"; 
+import App from "./App"; 
 
-const myComponent = <ShowList />;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(myComponent);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
